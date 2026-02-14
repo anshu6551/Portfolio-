11b363b545f8f323f5adfc0abdeeaf0de01169d7
Anshu kumari <anshu5196kumari@gmail.com>
2026-02-15 01:09:58 +0530

    last commit of saturday

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,96 +1,62 @@
-import { Box, Typography, Button, Container, Stack } from "@mui/material";
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import "../../App.css"
+import Image from "../../Image/figma pic.png"
 
-export default function Home() {
+const Home = () => {
   return (
-    <Box
-      sx={{
-        bgcolor: "#f2f2f2",
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        pt: 10,
-      }}
-    >
-      <Container maxWidth="lg">
-        <Stack direction={{ xs: "column", md: "row" }} spacing={5} alignItems="center">
+    <>
+      <Box sx={
+        {
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          flexDirection: "column",
+          pt: "140px"
           
-          {/* LEFT TEXT */}
-          <Box flex={1}>
-            <Typography
-              sx={{
-                bgcolor: "white",
-                display: "inline-block",
-                px: 2,
-                py: 0.5,
-                borderRadius: 20,
-                fontSize: 14,
-              }}
-            >
-              Hello!
-            </Typography>
 
-            <Typography
-              sx={{
-                fontWeight: 800,
-                fontSize: { xs: "2.2rem", md: "3.8rem" },
-                mt: 2,
-              }}
-            >
-              I'm <span style={{ color: "#ff8a00" }}>Jenny</span>, <br />
-              Product Designer
-            </Typography>
+        }
+      }>
+        <Box>
+          <Typography
+          sx={{
+            fontSize:"65px",
+            fontWeight: "600",
+            textAlign: "center",
+            lineHeight: "140%",
+            letterSpacing: "-1.5%"
+          }}>
+            I’m <span style={{
+              color: "#FD853A"
+            }}>Anshu</span> , <br /> Frontend Developer
+          </Typography>
 
-            <Typography sx={{ color: "gray", mt: 2 }}>
-              Jenny’s exceptional product design ensures our website’s success.
-              Highly recommended.
-            </Typography>
+        </Box>
+        <Box sx={
+          {
+            pt:"50px",
+          }
+        }>
+          <Box sx={{
+            width: "600px",
+            height: "600px",
+            borderRadius: "50%",
+            backgroundColor: "#FEB273",
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-start"
+          }}>
+            <Box>
+              <img src= {Image} />
+            </Box>
 
-            <Stack direction="row" spacing={2} mt={3}>
-              <Button
-                variant="contained"
-                sx={{
-                  bgcolor: "#ff8a00",
-                  borderRadius: 30,
-                  px: 3,
-                  "&:hover": { bgcolor: "#ff6f00" },
-                }}
-              >
-                Portfolio
-              </Button>
-
-              <Button variant="outlined" sx={{ borderRadius: 30 }}>
-                Hire Me
-              </Button>
-            </Stack>
           </Box>
-
-          {/* RIGHT IMAGE */}
-          <Box flex={1} sx={{ position: "relative", display: "flex", justifyContent: "center" }}>
-            {/* Orange Circle */}
-            <Box
-              sx={{
-                width: { xs: 250, md: 420 },
-                height: { xs: 250, md: 420 },
-                bgcolor: "#ff8a00",
-                borderRadius: "50%",
-                position: "absolute",
-                bottom: 0,
-              }}
-            />
-
-            {/* Image */}
-            <img
-              src="/jenny.png"   // put image in public folder
-              alt="Jenny"
-              style={{
-                width: "280px",
-                zIndex: 2,
-              }}
-            />
-          </Box>
-
-        </Stack>
-      </Container>
-    </Box>
-  );
+        </Box>
+      </Box>
+    </>
+  )
 }
+
+export default Home
